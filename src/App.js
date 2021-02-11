f97f0815ab343a6999bcca0bac145af4798d909f
patrick hrabos <phrabos@gmail.com>
2021-02-10 20:37:42 -0500

    refactor code to make components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import Header from './header.js';
 import './App.css';
 import React from 'react';
 import ImageList from './ImageList.js';
-import images from './data';
+import images from './data.js';
+import images2 from './data2';
+import Dropdown from './dropdown.js';
 
 export default class App extends React.Component {
   state = {
@@ -14,51 +16,42 @@ export default class App extends React.Component {
       keyword: e.target.value
     })
   }
+  handleHornChange = (e) =>{
+    this.setState({
+      horns: Number(e.target.value)
+    })
+  }
   render() {
-
-    // console.log(this.state.horns)
     const uniques = Array.from(new Set(images.map(image => image.keyword)));
-    const keywordArray = uniques.map(unique => (<option key={Math.random()} value={unique}>{unique}</option>))
+    
     const filteredCreatures = images.filter((image)=> {
       if (!this.state.keyword) return true;
       if (image.keyword === this.state.keyword) return true;
       return false;
     });
     const uniqueHorns = Array.from(new Set(filteredCreatures.map(image => image.horns)));
-    const hornsArray = uniqueHorns.map(horn => (<option key={Math.random()} value={horn}>{horn}</option>));
+
     const filteredByHorns = filteredCreatures.filter((animal)=> {
       
         if (!this.state.horns) return true;
-        if (animal.horns.toString() === this.state.horns) return true;
-        console.log(animal.horns, this.state.horns)
+        if (animal.horns === this.state.horns) return true;
         return false;
     })
 
     return (
       <div className="App">
         <Header />
-        <select 
-          value={this.state.keyword}
-          onChange={(e) => {
-            this.setState({
-              keyword: e.target.value
-            })
-          }} 
-          >
-          <option value={''}>- Choose A Category -</option>
-          {keywordArray}
-        </select><br/>
-        <select 
-          value={this.state.horns}
-          onChange={(e) => {
-            this.setState({
-              horns: e.target.value
-            })
-          }} 
-          >
-          <option value={''}>- No. of Horns -</option>
-          {hornsArray}
-        </select>
+        <Dropdown 
+          currentValue={this.state.keyword}
+          handleChange={this.handleNameChange}
+          options={uniques}
+        />
+        <Dropdown 
+          currentValue={this.state.horns}
+          handleChange={this.handleHornChange}
+          options={uniqueHorns}
+        />
+        <button>Reset Filter</button>
           <div className='image-div'>
             <ImageList images={filteredByHorns}/>
           </div>
@@ -67,4 +60,3 @@ export default class App extends React.Component {
     );
   };
 }
-
--- src/imageItems.js
+++ src/imageItems.js
@@ -4,11 +4,13 @@ import React from 'react';
 export default class ImageItems extends React.Component {
     render() {
 
-        const imagesArray = this.props.images.map(image => (<li key={Math.random()}><img className='images' src={image.url} alt={image.keyword}/></li>))
+        // const imagesArray = this.props.images.map(image => (<li key={Math.random()}><img className='images' src={image.url} alt={image.keyword}/></li>))
 
         return (
             <>
-                {imagesArray}
+                <li key={Math.random()}>
+                    <img className='images' src={this.props.url} alt={this.props.alt}/>
+                </li>
             </>
         )
     }
